9ed9eb4c3bd8d4e0866f27559912e6ece106f75f
Lucien Morey <lucienp@live.com.au>
2022-01-13 17:02:41 +1100

    added comments to sim

--- slimey_core/src/simulation/slime_sim.cpp
+++ slimey_core/src/simulation/slime_sim.cpp
@@ -13,16 +13,19 @@ SlimeSim::SlimeSim(
   int num_species, PositionMode pos_mode)
 : num_agents_(num_agents)
 {
+  // generate agents
   agent_generator =
     std::make_shared<AgentSystem>(win_width, win_height, num_agents_, num_species, pos_mode);
 
+  // create simulation compute shader
   const auto agentShaderSrc = physarum::readFile(shader_dir / "agent.comp.glsl");
   if (!agentShaderSrc.has_value()) {
-    throw std::runtime_error("Could not load 'physarum_sim.comp'");
+    throw std::runtime_error("Could not load 'agent.comp.glsl'");
   }
   agentShader = std::make_shared<Shader>(GL_COMPUTE_SHADER, agentShaderSrc.value());
   agentComputeProgram = std::make_shared<Program>(agentShader);
 
+  // bind  agent buffer to the simulation
   agentBuffer = std::make_shared<Buffer>(num_agents_ * sizeof(Agent), nullptr, GL_DYNAMIC_DRAW);
   agentBuffer->setData(agent_generator->getAgents());
 }
@@ -33,6 +36,7 @@ void SlimeSim::init() {}
 
 void SlimeSim::simulate(float delta_time)
 {
+  //update delta time and dispatch next iteration of thread
   agentComputeProgram->use();
   agentComputeProgram->set1f("deltaTime", delta_time);
   agentBuffer->bindBase(GL_SHADER_STORAGE_BUFFER, 2);
