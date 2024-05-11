d4c33d24e632a2f1e8c6575ee8356790d779d229
Lucien Morey <lucienp@live.com.au>
2024-05-11 16:23:12 +1000

    create vertex buffer class

---
+++ include/gl_wrapper/gl_wrapper/vertex_buffer.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <GL/glew.h>
+
+#include <span>
+
+namespace GlWrapper
+{
+
+template <class T>
+class VertexBuffer
+{
+public:
+  VertexBuffer() = delete;
+
+  VertexBuffer(const std::span<T> buffer)
+  {
+    // check lib types match inbuilt types
+    static_assert(sizeof(uint32_t) == sizeof(GLuint), "GLuint is expected to be 32bits in size");
+
+    glGenBuffers(1, &id_);
+    bind();
+    glBufferData(GL_ARRAY_BUFFER, buffer.size() * sizeof(T), buffer.data(), GL_STATIC_DRAW);
+  }
+
+  ~VertexBuffer() { glDeleteBuffers(1, &id_); }
+  void bind() const { glBindBuffer(GL_ARRAY_BUFFER, id_); }
+  void unbind() const { glBindBuffer(GL_ARRAY_BUFFER, 0); }
+
+private:
+  uint32_t id_;
+};
+
+};  // namespace GlWrapper
