32c68d06cf6830d3a70cfd3e32b9407d02876324
Lucien Morey <lucienp@live.com.au>
2024-05-11 16:23:43 +1000

    draw basic triangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include <gl_wrapper/vertex_buffer.hpp>
 #include <iostream>
 
 int main()
@@ -28,11 +29,23 @@ int main()
     return -2;
   }
 
+  // An array of 3 vectors which represents 3 vertices of a triangle
+  static std::array<GLfloat, 6> triangle_vertex_positions = {-1.0f, -1.0f, 1.0f, -1.0f, 0.0f, 1.0f};
+
+  GlWrapper::VertexBuffer<GLfloat> triangle_buffer(triangle_vertex_positions);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 2, (const void *)0);
+  triangle_buffer.unbind();
+
   /* Loop until the user closes the window */
   while (!glfwWindowShouldClose(window)) {
     /* Render here */
     glClear(GL_COLOR_BUFFER_BIT);
 
+    // Draw the triangle !
+    triangle_buffer.bind();
+    glDrawArrays(GL_TRIANGLES, 0, 3);  // Starting from vertex 0; 3 vertices total -> 1 triangle
+    triangle_buffer.unbind();
     /* Swap front and back buffers */
     glfwSwapBuffers(window);
 
