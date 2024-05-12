898b520b620855ea0d311b70edad0ffcee5e37ec
Lucien Morey <lucienp@live.com.au>
2024-05-12 15:09:09 +1000

    initialise shaders to turn triangle green

---
+++ include/gl_wrapper/gl_wrapper/shader.hpp
@@ -0,0 +1,61 @@
+#pragma once
+#include <GL/glew.h>
+
+#include <string>
+
+namespace GlWrapper
+{
+
+class Shader
+{
+public:
+  Shader() = delete;
+
+  Shader(const uint32_t type, const std::string & source)
+  {
+    static_assert(sizeof(uint32_t) == sizeof(GLuint), "GLuint is expected to be 32bits in size");
+
+    id_ = glCreateShader(type);
+    source_ = source;
+  }
+  ~Shader() { glDeleteShader(id_); }
+
+  bool compile() const
+  {
+    const char * raw_source = source_.data();
+    glShaderSource(id_, 1, &raw_source, nullptr);
+    glCompileShader(id_);
+    return has_compiled();
+  }
+
+  std::string get_compilation_log() const
+  {
+    int length;
+    glGetShaderiv(id_, GL_INFO_LOG_LENGTH, &length);
+
+    std::string message;
+
+    if (length != 0) {
+      message.resize(length);
+      glGetShaderInfoLog(id_, length, &length, message.data());
+    }
+
+    return message;
+  }
+
+  uint32_t get_id() const { return id_; }
+
+  bool has_compiled() const
+  {
+    int32_t result;
+    glGetShaderiv(id_, GL_COMPILE_STATUS, &result);
+
+    return result == GL_TRUE;
+  }
+
+private:
+  uint32_t id_;
+  std::string source_;
+};
+
+};  // namespace GlWrapper
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include <fstream>
+#include <gl_wrapper/shader.hpp>
+#include <gl_wrapper/shader_program.hpp>
 #include <gl_wrapper/vertex_buffer.hpp>
 #include <iostream>
+#include <sstream>
 
 int main()
 {
@@ -36,6 +40,24 @@ int main()
   glEnableVertexAttribArray(0);
   glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 2, (const void *)0);
   triangle_buffer.unbind();
+
+  std::ifstream fragment_file("shaders/fragment_shader.glsl");
+  std::stringstream fragment_source;
+  fragment_source << fragment_file.rdbuf();
+  GlWrapper::Shader fragment_shader(GL_FRAGMENT_SHADER, fragment_source.str());
+  fragment_shader.compile();
+
+  std::ifstream vertex_file("shaders/vertex_shader.glsl");
+  std::stringstream vertex_source;
+  vertex_source << vertex_file.rdbuf();
+  GlWrapper::Shader vertex_shader(GL_VERTEX_SHADER, vertex_source.str());
+  vertex_shader.compile();
+
+  GlWrapper::ShaderProgram program;
+  program.attach_shader(fragment_shader);
+  program.attach_shader(vertex_shader);
+  program.link();
+  program.bind();
 
   /* Loop until the user closes the window */
   while (!glfwWindowShouldClose(window)) {
